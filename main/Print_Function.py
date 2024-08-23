0dfacfff7cdc5fa5cf95f07ca59ded043127cbf2
LasyaSriVanga <159695552+LasyaSriVanga@users.noreply.github.com>
2024-08-23 18:38:27 +0530

    Add Python solutions to help earn the silver badge
    This commit adds Python solutions to a selection of basic HackerRank problems. These solutions are designed to be straightforward and easy to understand, providing a solid foundation for beginners. By solving these problems, users can earn a silver badge for Python on HackerRank.
    
    The solutions are organized in the `main` directory and are written with clear syntax and adherence to problem instructions. This set of solutions aims to help users improve their problem-solving skills and familiarize themselves with fundamental Python concepts.
    
    Feel free to explore, modify, and use these solutions as a reference. Contributions and improvements are welcome!

---
+++ main/Print_Function.py
@@ -0,0 +1,30 @@
+# The included code stub will read an integer, n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "..." represents the consecutive values in between.
+
+# Example :
+# n = 5
+# Print the string 12345.
+
+# Input Format :
+# The first line contains an integer .
+
+# Constraints :
+# 1<=n<=150
+
+# Output Format :
+# Print the list of integers from 1 through n as a string, without spaces.
+
+# Sample Input 0 :
+# 3
+
+# Sample Output 0 :
+# 123
+
+n = int(input())
+if 1<=n<=150:
+    i=1
+    while i<=n:
+        print(i,end="")
+        i+=1
